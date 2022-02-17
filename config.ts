917e4d3b265a905e79cd698e7fbc68ebaf4ebdb6
Viktor Ehinlanwo <ehinlanwo.deji@live.dk>
2022-02-17 13:15:22 +0100

    adjusted config file

--- config.ts
+++ config.ts
@@ -10,7 +10,7 @@ class config{
     constructor(){
         this.databaseConnection = process.env.CONNECTION_STRING
         this.origins = process.env.ORIGINS.split(" ");
-        this.port = process.env.PORT;
+        this.port = parseInt(process.env.PORT);
         this.paymentAPI = process.env.PAYMENT_API;
     }
 }
